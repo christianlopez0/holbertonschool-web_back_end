0bfce0eff38c6ae9143942eb48948a18f3f97eeb
Christian lopez <christianlopezrivera0@gmail.com>
2024-11-13 16:44:59 -0500

    Salvando el semestre

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -4,14 +4,17 @@ More involved type annotations for safely retrieving a value from a dictionary.
 """
 from typing import Mapping, Any, Union, TypeVar
 
-T = TypeVar('T', bound=Any)  
+T = TypeVar('T')
+
 
 def safely_get_value(
-    dct: Mapping[Any, Any],  
-    key: Any,                
-    default: Union[T, None] = None  
-) -> Union[Any, T]:         
+    dct: Mapping[Any, Any],
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
     """
     Safely get a value from the dictionary, returning the default if the key is not found.
     """
-    return dct.get(key, default)
+    if key in dct:
+        return dct[key]
+    return default
