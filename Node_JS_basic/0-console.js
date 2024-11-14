bce69f4e543c6805b273019724d0db5cce15be84
Christian Lopez <christianlopezrivera0@gmail.com>
2024-11-13 20:39:55 -0800

    salvando semestre

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,7 +1,5 @@
-// 0-console.js
-
-function displayMessage(message) {
-    console.log(message);
-}
-
-module.exports = displayMessage;
+function displayMessage(string) {
+    console.log(string);
+  }
+  
+  module.exports = displayMessage;
--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,23 +1,15 @@
-// 1-stdin.js
-
-const readline = require('readline');
-
-// Create an interface for input and output
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-// Display the welcome message
-console.log("Welcome to Holberton School, what is your name?");
-
-// Capture the user input
-rl.on('line', (input) => {
-    console.log(`Your name is: ${input}`);
-    rl.close(); // Close the interface after the input
-});
-
-// Handle the close event
-rl.on('close', () => {
-    console.log("This important software is now closing");
+console.log('Welcome to Holberton School, what is your name?');
+// await input
+process.stdin.resume();
+// once input is entered
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  // check if input is coming from the terminal
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
